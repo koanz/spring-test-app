9e8b36aa90dffcbf75dba9a2aa1385e7cdaa7f43
Cristian <cristiananzawa@gmail.com>
2024-12-11 12:16:47 -0300

    populate data with import file in h2

---
+++ src/test/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO accounts (person, balance) VALUES ('John Doe', 1000);
+INSERT INTO accounts (person, balance) VALUES ('Jane Doe', 2000);
+INSERT INTO banks (name, total_transfer) VALUES ('Santander Rio', 0);
