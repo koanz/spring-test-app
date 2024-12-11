31aadf06d38ac56415cbd5dc06289c87b9e929b0
Cristian <cristiananzawa@gmail.com>
2024-12-11 19:40:33 -0300

    removing springfox starter dependency and comment on SpringFoxConfig annotation

--- src/main/java/com/koanz/test/springboot/app/configs/SpringFoxConfig.java
+++ src/main/java/com/koanz/test/springboot/app/configs/SpringFoxConfig.java
@@ -2,21 +2,20 @@ package com.koanz.test.springboot.app.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
+/*import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.Docket;*/
 
-@Configuration
+//@Configuration
 public class SpringFoxConfig {
 
-    @Bean
+    /*@Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.koanz.test.springboot.app.controllers"))
-                .paths(PathSelectors.ant("/api/accounts/*"))
-                .build();
-    }
+                .paths(PathSelectors.ant("/api/cuentas/*")).build();
+    }*/
 
 }
